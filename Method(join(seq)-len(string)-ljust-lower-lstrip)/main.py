b3532460ac6dfb3e88b7a8d9a9a85c37b81e5731
Mohini-2002 <77198245+Mohini-2002@users.noreply.github.com>
2021-04-18 08:43:40 -0700

    Method(join, len, ljust, lower, lstrip)

---
+++ Method(join(seq)-len(string)-ljust-lower-lstrip)/main.py
@@ -0,0 +1,32 @@
+#join(seq) and split  and its type
+st1 = input("Enter a string :")
+t1 = st1.split(" ")
+print(t1, type(t1))
+s1 = " ".join(t1)
+print(s1, type(s1))
+
+
+#len(string) and its type
+st2 = input("Enter a string :")
+t2 = len(st2)
+print(t2, type(t2))
+
+#ljust and its type
+st3 = input("Enter a string :")
+t3 = st3.ljust(20, "*")
+print(t3, type(t3))
+
+#lower and its type
+st4 = input("Enter a string :")
+t4 = st4.lower()
+print(t4, type(t4))
+
+#lstrip and its type
+st5 = input("Enter a string :")
+t5 = st5.lstrip("O")
+print(t5, type(t5))
+
+
+
+
+
