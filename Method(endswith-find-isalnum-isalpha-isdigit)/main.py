ba03e9b32d88b769d180e172590d8b8370561c36
Mohini-2002 <77198245+Mohini-2002@users.noreply.github.com>
2021-04-18 08:33:23 -0700

    Method(endswith, find, isalnum, isalpha, isdigit)

---
+++ Method(endswith-find-isalnum-isalpha-isdigit)/main.py
@@ -0,0 +1,24 @@
+#Endswith() and its class
+ST1 = input("Enter a string :")
+ST1 = ST1.endswith("ing")
+print(ST1, type(ST1))
+
+#Find() and its class or type
+ST2 = input("Enter a string :")
+ST2 = ST2.find("ING")
+print(ST2, type(ST2))
+
+#Isalnum() and its type
+ST3 = input("Enter a string :")
+ST3 = ST3.isalnum()
+print(ST3, type(ST3))
+
+#Isalpha() and its type
+ST4 = input('Enter a string :')
+ST4 = ST4.isalpha()
+print(ST4, type(ST4))
+
+#Isdigit() and its type
+ST5 = input("Enter a string:")
+ST5 = ST5.isdigit()
+print(ST5, type(ST5))
