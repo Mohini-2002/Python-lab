fcb0ccf695088f03581177bb5b8cd3fbf30e3c51
Mohini-2002 <77198245+Mohini-2002@users.noreply.github.com>
2021-04-18 08:19:20 -0700

    Single character access from user

---
+++ CharacterAccessByIndexingAndSlicing/main.py
@@ -0,0 +1,12 @@
+#Character access by indexing and slicing
+#By indexing
+ST1 = input("Enter the string :")
+ST1 = ST1[0]
+print(ST1, type(ST1))
+#By slicing
+ST2 = input("Enter the string :")
+ST2 = ST2[:3:]
+print(ST2, type(ST2))
+
+
+
